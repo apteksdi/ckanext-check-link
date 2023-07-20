0e3d0a987c8d7d8daed627586867439adc26d5f5
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-07-20 19:52:25 +0300

    feat: delete reports when resource deleted via `ckanext.check_link.remove_reports_when_resource_deleted`

--- ckanext/check_link/plugin.py
+++ ckanext/check_link/plugin.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 
+from typing import Any
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan import model
 
 from . import cli, helpers, views
 from .logic import action, auth
+from .model import Report
+
+CONFIG_CASCADE_DELETE = "ckanext.check_link.remove_reports_when_resource_deleted"
 
 
 class CheckLinkPlugin(plugins.SingletonPlugin):
@@ -14,8 +20,15 @@ class CheckLinkPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.IClick)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IDomainObjectModification, inherit=True)
+
+    def notify(self, entity: Any, operation: str) -> None:
+        if isinstance(entity, model.Resource) and entity.state == "deleted":
+            if toolkit.asbool(toolkit.config.get(CONFIG_CASCADE_DELETE)):
+                _remove_resource_report(entity.id)
 
     # ITemplateHelpers
+
     def get_helpers(self):
         return helpers.get_helpers()
 
@@ -40,3 +53,9 @@ class CheckLinkPlugin(plugins.SingletonPlugin):
     # IClick
     def get_commands(self):
         return cli.get_commands()
+
+
+def _remove_resource_report(resource_id: str):
+    report = Report.by_resource_id(resource_id)
+    if report:
+        model.Session.delete(report)
