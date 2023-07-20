1a39d7b07318336ba2fd42f1019ec51ad87c15de
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-07-20 19:16:35 +0300

    fix: add cascade removal of reports

--- ckanext/check_link/tests/model/test_report.py
+++ ckanext/check_link/tests/model/test_report.py
@@ -8,13 +8,17 @@ from ckanext.check_link.model import Report
 @pytest.mark.usefixtures("with_plugins", "clean_db")
 class TestReport:
     def test_removed_with_resource(self, resource, faker):
+        res = model.Resource.get(resource["id"])
+
         report = Report(url=faker.url(), resource_id=resource["id"], state="unknown")
         model.Session.add(report)
         model.Session.commit()
 
         assert report.id
 
-        model.Session.delete(model.Resource.get(resource["id"]))
+        res = model.Resource.get(resource["id"])
+
+        model.Session.delete(res)
         model.Session.commit()
         assert not model.Session.query(Report).filter_by(id=report.id).one_or_none()
         assert not Report.by_resource_id(resource["id"])
